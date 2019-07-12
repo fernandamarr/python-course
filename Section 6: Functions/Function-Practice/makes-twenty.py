4091073c6b4533ed0e7ba32cecfa8671f6686a52
Fernanda <fernanda.marroquin07@gmail.com>
2019-07-12 11:56:39 -0400

    makes twenty solved

---
+++ Section 6: Functions/Function-Practice/makes-twenty.py
@@ -0,0 +1,10 @@
+# MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
+# makes_twenty(20,10) --> True
+# makes_twenty(12,8) --> True
+# makes_twenty(2,3) --> False
+
+def makes_twenty(n1,n2):
+    if (n1 == 20 or n2 == 20) or n1 + n2 == 20:
+        return True
+    else:
+        return False
