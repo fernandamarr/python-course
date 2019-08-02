e56d7ee3ff0d09bb90aaf255a3706c3ffe59f0f8
Fernanda <fernanda.marroquin07@gmail.com>
2019-08-02 13:32:58 -0400

    makes twenty solved

--- Section 6: Functions/Function-Practice/makes-twenty.py
+++ Section 6: Functions/Function-Practice/makes-twenty.py
@@ -4,7 +4,4 @@
 # makes_twenty(2,3) --> False
 
 def makes_twenty(n1,n2):
-    if (n1 == 20 or n2 == 20) or n1 + n2 == 20:
-        return True
-    else:
-        return False
+    return (n1 == 20 or n2 == 20) or n1 + n2 == 20
