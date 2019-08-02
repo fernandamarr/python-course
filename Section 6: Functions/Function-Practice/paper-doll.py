376a3ee7d0f0a6665b7d1188235949ad29623c8f
Fernanda <fernanda.marroquin07@gmail.com>
2019-08-02 14:27:35 -0400

    paper doll fix

---
+++ Section 6: Functions/Function-Practice/paper-doll.py
@@ -0,0 +1,9 @@
+# PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
+# paper_doll('Hello') --> 'HHHeeellllllooo'
+# paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'
+
+def paper_doll(text):
+    result = ''
+    for elem in text:
+        result += elem * 3
+    return result
